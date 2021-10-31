5177ac0a9f52b54c50ca7b0bfb58869e73bcc0fa
benjamin <benjinoble@gmail.com>
2021-11-01 09:23:01 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+const element = document.createElement('div');
+
+document.body.appendChild(element);
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = (i + 1).toString();
+    ul.appendChild(li);
+}
+element.appendChild(ul);
+
+element.style.fontSize = `18px`;
+element.style.color = `red`;
+element.style.lineHeight = `2`;
+
+document.getElementById("main").remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = `victory`;
+newHeader.innerHTML = `<h1>Benjamin Noble is the champion</h1>`;
